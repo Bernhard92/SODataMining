d0178b9b90573437e14036ccc4e6f6233b265250
Bernhard <bejahrer@edu.aau.at>
2018-11-04 00:26:39 +0100

    added first classes

---
+++ sodatamining/DBConnection.py
@@ -0,0 +1,51 @@
+'''
+Created on 3 Nov 2018
+
+@author: Bernhard
+'''
+import mysql.connector
+from mysql.connector import Error
+
+class DBConnection:
+        
+    def __init__(self):
+        self.conn = self.connect()
+         
+    def connect(self):
+        """ Connect to MySQL database """
+        try:
+            conn = mysql.connector.connect(host='localhost',
+                                           database='python_mysql',
+                                           user='root',
+                                           password='122133144')
+            if conn.is_connected():
+                print('Connected to MySQL database')
+            return conn
+        except Error as e:
+            print(e)
+     
+        
+    def get_post_with_id(self, id):
+        try:
+            cursor = self.conn.cursor()
+            cursor.execute("SELECT * FROM posts WHERE `id` = id")
+     
+            row = cursor.fetchone()
+            
+            while row is not None:
+                print(row)
+                row = cursor.fetchone()
+     
+        except Error as e:
+            print(e)
+            
+        finally:
+            cursor.close()
+               
+            
+            
+    def close_connection(self):
+        self.conn.close() 
+        
+        
+             
---
+++ sodatamining/Readability.py
@@ -0,0 +1,27 @@
+import readability
+import DBConnection
+
+
+class Readability:
+    
+    def __init__(self):
+        text = ('This is an example sentence .\n'
+        'Note that tokens are separated by spaces and sentences by newlines .\n')
+        
+        results = readability.getmeasures(unicode(text), lang='en')
+        print(results['readability grades']['FleschReadingEase'])
+        
+        self._dbConnection = DBConnection()
+
+    @property
+    def dbConnection(self):
+        return self._dbConnection
+    
+    def test(self):
+        print("s")
+        
+    def get_post_from_db(self, id):
+        self.dbConnection.get_post_with_id(id)
+    
+        
+    
